a5d6036f2355c670c5270ac8ad48ac7291d36bec
Abhijit Majumder <ts-abhiji.a.majumder@rakuten.com>
2020-05-07 12:13:16 +0900

    Added Image Lazy load

--- movie-finder/src/Poster/index.js
+++ movie-finder/src/Poster/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
 import defaultImage from '../Images/def.jpg';
 
 export default function Poster(props) {
   const { imagePath } = props;
   return (
-    <img
-      alt="poster"
-      className="poster"
-      src={
-        imagePath
-          ? `https://image.tmdb.org/t/p/w300_and_h450_bestv2${imagePath}`
-          : defaultImage
-      }
-    ></img>
+    <LazyLoad className="poster" debounce={false} offsetVertical={500}>
+      <img
+        alt="poster"
+        className="poster"
+        src={
+          imagePath
+            ? `https://image.tmdb.org/t/p/w300_and_h450_bestv2${imagePath}`
+            : defaultImage
+        }
+      />
+    </LazyLoad>
   );
 }
